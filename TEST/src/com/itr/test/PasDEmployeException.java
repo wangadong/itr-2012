ab6772f7ffdef85bb0b048091978ecfa99c29523
wangadong <tonywangadong@gmail.com>
2012-10-12 00:34:34 +0800

    copied Exception Class from Professor

---
+++ TEST/src/com/itr/test/PasDEmployeException.java
@@ -0,0 +1,18 @@
+package com.itr.test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Tony
+ * 
+ */
+public class PasDEmployeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2425478859507752180L;
+
+}
